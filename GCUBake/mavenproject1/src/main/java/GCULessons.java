8b71f5de70a904c05bd8dcba5e583058a6e2161d
ARazak200 <58515893+ARazak200@users.noreply.github.com>
2020-02-22 17:53:17 +0000

    getters and setters

--- GCUBake/mavenproject1/src/main/java/GCULessons.java
+++ GCUBake/mavenproject1/src/main/java/GCULessons.java
@@ -10,4 +10,45 @@
  */
 public class GCULessons {
     
+    private String type;
+    private String lessonID;
+    private String name;
+    private String status;
+
+    
+    //Getters and setters
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLessonID() {
+        return lessonID;
+    }
+
+    public void setLessonID(String lessonID) {
+        this.lessonID = lessonID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+    
 }
