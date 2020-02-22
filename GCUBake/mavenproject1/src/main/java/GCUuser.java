0b95fce5f788d25611609174ed7f484c8b2505ea
ARazak200 <58515893+ARazak200@users.noreply.github.com>
2020-02-22 19:22:14 +0000

    Aadam's update
    added getters and setters
    
    methods and comments

--- GCUBake/mavenproject1/src/main/java/GCUuser.java
+++ GCUBake/mavenproject1/src/main/java/GCUuser.java
@@ -1,42 +1,121 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This class will handle the users and who has permission for certain functuon 
  */
 
 /**
  *
- * @author timenos
+ * @author timenos , aadamrazak
  */
 public class GCUuser {
     
-    private String username;
+    //Variables
     private String title;
     private String firstName;
     private String lastName;
     private int contactNo;
+    
+    
+    private String username;
     private int passCode;
     private String gcuRole;
-    
-    public static void main(String[]args){
-        
-
-
-
-}
 
     
+    //Getters
+    public String getTitle() {
+        return title;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getContactNo() {
+        return contactNo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getPassCode() {
+        return passCode;
+    }
+
+    public String getGcuRole() {
+        return gcuRole;
+    }
+//Setters**Check if you need all of them or some of them
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setContactNo(int contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassCode(int passCode) {
+        this.passCode = passCode;
+    }
+
+    public void setGcuRole(String gcuRole) {
+        this.gcuRole = gcuRole;
+    }
     
-public GCUuser(){
-    this.username = username;
     
+    //Not need this only for run class
+//    public static void main(String[]args){
+//        
+//
+//
+//
+//}
+    
+    
+//Default Contructor
+    GCUuser()
+    {}
+    
+//overloaded Contructor
+    
+    public GCUuser(String pTitle, String pFirstName, String pLastName, int pContactNo, String pUsername, int pPassCode, String pGcuRole) {
+        this.title = pTitle;
+        this.firstName = pFirstName;
+        this.lastName = pLastName;
+        this.contactNo = pContactNo;
+        this.username = pUsername;
+        this.passCode = pPassCode;
+        this.gcuRole = pGcuRole;
+    }
+    
+    
+//Methods
+public void logIn()
+{}
 
+public void Register()
+{}
 
+public void DeleteUser()
+{}
 
-}
 
   
     
-}
+}//end of class
 
